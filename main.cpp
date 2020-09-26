d4d40dc442d1e7bce02b55879d82d1eb7fa24c3c
Snežana Nikonova <71488175+Snezana01@users.noreply.github.com>
2020-09-26 23:00:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+struct duomuo {
+    string Vard;
+    string Pav;
+    int paz[10];
+    int egz;
+    float GP = 0;
+};
+
+int main()
+{
+    duomuo Eil; duomuo Eil_mas[5];
+    vector<duomuo> Eil_vect;
+    int n;
+    cout<<"Iveskite kiek studentu ";
+    cin>>n;
+    string vardas, pavarde;
+    int integer;
+    for(int i=0;i<n;i++)
+    {
+        Eil_vect.push_back(duomuo());
+        cin>>vardas;
+        cin>>pavarde;
+        Eil_vect[i].Vard = vardas;
+        Eil_vect[i].Pav = pavarde;
+        cin>>integer;
+        Eil_vect[i].egz = integer;
+        for (int j = 0; j < 5; j++) 
+        {
+            cin>>integer;
+            Eil_vect[i].paz[j]=integer;
+            Eil_vect[i].GP+=Eil_vect[i].paz[j];
+        }
+        Eil_vect[i].GP = Eil_vect[i].GP / 5.0;
+        Eil_vect[i].GP = Eil_vect[i].GP * 0.4 + 0.6 * Eil_vect[i].egz;
+    }
+    //ivedimas virsuj
+    int bonus_tarpai1, bonus_tarpai2;
+    for(int i=0;i<n;i++)
+    {
+      bonus_tarpai1 = 10-Eil_vect[i].Vard.length();
+      bonus_tarpai2 = 10-Eil_vect[i].Pav.length();
+      cout << Eil_vect[i].Vard;
+       for(int j=0;j<bonus_tarpai1;j++)
+       {
+         cout<<" ";
+       } 
+       cout << Eil_vect[i].Pav;
+       for(int j=0;j<bonus_tarpai2;j++)
+       {
+         cout<<" ";
+       } 
+       cout<<Eil_vect[i].GP<<std::endl;
+    }
+}
