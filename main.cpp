3a0b2d2c2a835647a9af5b3323004f039e4f55fc
Snežana Nikonova <71488175+Snezana01@users.noreply.github.com>
2020-09-27 15:11:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include <bits/stdc++.h>
 
 using std::cout;
 using std::cin;
@@ -23,40 +25,58 @@ int main()
     cin>>n;
     string vardas, pavarde;
     int integer;
+    int pazym;
+    int mediana[15]={0};
+    double medianai;
     for(int i=0;i<n;i++)
     {
         Eil_vect.push_back(duomuo());
+        cout<<"Iveskite varda ir pavarde ";
         cin>>vardas;
         cin>>pavarde;
         Eil_vect[i].Vard = vardas;
         Eil_vect[i].Pav = pavarde;
+        cout<<"Iveskite egzamino paz. ";
         cin>>integer;
+        cout<<"Iveskite kiek pazymiu bus "; 
+        int k;
+        cin>>k;
+        cout<<"Iveskite " <<k << " pazymiu/us ";
         Eil_vect[i].egz = integer;
-        for (int j = 0; j < 5; j++) 
+        mediana[0]=integer;
+        for (int j = 0; j < k; j++) 
         {
             cin>>pazym;
             Eil_vect[i].paz[j]=pazym;
             Eil_vect[i].GP+=Eil_vect[i].paz[j];
+            mediana[j+1]= pazym;
         }
-        Eil_vect[i].GP = Eil_vect[i].GP / 5.0;
+        
+        Eil_vect[i].GP = Eil_vect[i].GP / (k+1)/1.0;
         Eil_vect[i].GP = Eil_vect[i].GP * 0.4 + 0.6 * Eil_vect[i].egz;
+        //medianai skaiciuot
+        for (int g=0; g<(k+1); g++) {
+          cout<<mediana[g]<<" ";
+        }
+        cout<<std::endl;
+        std::sort(mediana,mediana + (k+1));
+        if ((k+1) % 2 != 0) 
+        medianai = mediana[k / 2]; 
+        else medianai = (mediana[(k - 1) / 2] + mediana[(k+1) / 2]) / 2.0;
+
     }
     //ivedimas virsuj
-    int bonus_tarpai1, bonus_tarpai2;
+    cout<< "Iveskite ka norite pamatyti: jei mediana iveskite 'M', jei galutini rezultata iveskite 'R' ";
+    string ats;
+    cin>>ats;
+    cout << std::left << std::setw(15)<< "Vardas" <<std::left <<std::setw(15)<< "Pavarde ";
+    if (ats == "R") cout<<"Galutinis paz"<<std::endl;
+    else if (ats =="M") cout<<"Mediana"<<std::endl;
+    cout<<"___________________________________________"<<std::endl;
     for(int i=0;i<n;i++)
-    {
-      bonus_tarpai1 = 10-Eil_vect[i].Vard.length();
-      bonus_tarpai2 = 10-Eil_vect[i].Pav.length();
-      cout << Eil_vect[i].Vard;
-       for(int j=0;j<bonus_tarpai1;j++)
-       {
-         cout<<" ";
-       } 
-       cout << Eil_vect[i].Pav;
-       for(int j=0;j<bonus_tarpai2;j++)
-       {
-         cout<<" ";
-       } 
-       cout<<Eil_vect[i].GP<<std::endl;
+    { 
+      cout << std::left << std::setw(15)<< Eil_vect[i].Vard <<std::left <<std::setw(15)<< Eil_vect[i].Pav;
+      if (ats == "R") cout<< Eil_vect[i].GP <<std::endl;
+      else if (ats =="M") cout<< medianai <<std::endl;
     }
 }
